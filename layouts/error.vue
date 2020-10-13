e5eb917f44ea94f7cf844a28a4bc5d6391844cbc
Kovalchuk98 <alexander.kovalchuk98@gmail.com>
2020-10-13 11:24:09 +0300

    first commit

---
+++ layouts/error.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app dark>
+    <h1>{{ pageNotFound }}</h1>
+  </v-app>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['error'],
+  data () {
+    return {
+      pageNotFound: '404 Not Found',
+      otherError: 'An error occurred'
+    }
+  },
+  head () {
+    const title =
+    // this.error?.statusCode
+    this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+  color: white;
+}
+</style>
